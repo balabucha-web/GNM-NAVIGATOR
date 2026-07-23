1e542c8b098528ee40a20c0fc540ffb091e0762a
balabucha-web <balabucha@googlemail.com>
2026-07-23 16:30:25 +0200

    chore(reisepilot): bump version to 5.1 build 23

--- reisepilot/app/build.gradle.kts
+++ reisepilot/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.balabucha.reisepilot"
         minSdk = 27
         targetSdk = 37
-        versionCode = 22
-        versionName = "5.0"
+        versionCode = 23
+        versionName = "5.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
