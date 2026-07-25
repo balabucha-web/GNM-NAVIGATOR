bbbf7793bc72607fdf2d6f4ea80eb2faa2301293
balabucha-web <balabucha@googlemail.com>
2026-07-25 06:46:26 +0200

    Bump ReisePilot to 5.6 Build 29

--- reisepilot/app/build.gradle.kts
+++ reisepilot/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.balabucha.reisepilot"
         minSdk = 27
         targetSdk = 37
-        versionCode = 28
-        versionName = "5.5.1"
+        versionCode = 29
+        versionName = "5.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
